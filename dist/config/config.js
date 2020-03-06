0a362d0437da5b0e093771c1296346453a3e3b23
victor Naranjo <victornar97@gmail.com>
2020-03-06 13:10:34 -0400

    chat app development version backend

--- dist/config/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    MONGODB_URI: process.env.MONGODB_URI || '',
-    JWT_KEY: process.env.JWT_KEY || ''
-};
--- dist/models/connected.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var connectedSchema = new mongoose_1.Schema({
-    userName: { type: String, unique: true, required: true },
-});
-exports.default = mongoose_1.model('connected', connectedSchema);
